032dd1debde8309522e376d25129ca5925225ea4
kimtaemin <xoalstjtm@gmail.com>
2021-07-12 11:30:39 +0900

    랜덤 포트 부여 및 확인

--- src/main/java/com/example/firstservice/FirstServiceController.java
+++ src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,13 +1,24 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController
 {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Weclome to the First Service";
@@ -20,7 +31,10 @@ public class FirstServiceController
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "hi there, this is a message from first Service";
+    public String check(HttpServletRequest request)
+    {
+        log.info("server port:{}",request.getServerPort());
+        return String.format("hi there, this is a message from first Service on Port %s"
+                , env.getProperty("local.server.port"));
     }
 }
